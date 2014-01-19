8e9f3b6f13ba89f1e312d0b5fecf95ac0fe7c33a
Colin Shea <colin@evaryont.me>
2014-01-18 19:53:46 -0700

    what position are we in? Let's find out!

---
+++ scores.rb
@@ -0,0 +1,16 @@
+require 'json'
+require 'open-uri'
+require 'pp'
+
+our_points = 0
+our_rank = 0
+
+scores = JSON.parse(open('https://2014.ghostintheshellcode.com/ajax/getScores?limit=100').read)['scores']
+scores.each_with_index do |team, index|
+    if team['name'] == 'Pi Backwards'
+        our_points = team['points']
+        our_rank = index
+    end
+end
+
+puts "We're rank #{our_rank} with #{our_points} points! KEEP GOING!"
